1a8c8f1cb359d7cd396aa9a92337e0726939ebfc
ChiefCharpa <145765779+ChiefCharpa@users.noreply.github.com>
2024-04-14 14:50:31 +0100

    updated

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -138,15 +138,46 @@ namespace CMP1903_A1_2324
             }
         }
 
-        static void SevenOut()
+        public int SevenOut(bool player)
         {
+            _storedRolls = new List<int> { };
             //Rolls 2 dice
             Die die = new Die();
-
-            for (int i = 0; i<2;  i++)
+            int dieTotal = 0;
+            bool gameStop = false;
+            int counter = 0;
+            while (!gameStop)
             {
+                Console.WriteLine("Roll the dice");
+                Console.ReadLine();
                 die.Roll();
+                counter++;
                 int DieRoll1 = die.rollNumber;
+                die.Roll();
+                int DieRoll2 = die.rollNumber;
+
+                Console.WriteLine($"Die pair {counter} rolled {DieRoll1} and {DieRoll2}");
+
+                if (DieRoll1 + DieRoll2 != 7)
+                {
+                    _storedRolls.Add(DieRoll1);
+                    _storedRolls.Add(DieRoll2);
+                    if (DieRoll1 == DieRoll2)
+                    {
+                        dieTotal = dieTotal + 2 * (DieRoll1 + DieRoll2);
+                    }
+                    else
+                    {
+                        dieTotal = dieTotal + (DieRoll1 + DieRoll2);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The pair totalled to be 7.");
+                    Console.WriteLine($"You scored a total of {dieTotal}");
+                    break;
+                }
+                return dieTotal;
             }
             
 
@@ -154,54 +185,8 @@ namespace CMP1903_A1_2324
 
         static void Main(string[] args)
         {
-            // initially initialises a boolean variable valid setting it to false
-            bool valid = false;
-            // Whilst the value is false iterate through the code
-            while (!valid)
-            {
-                // A string variable InputtedValue is instantiated as an empty string
-                string inputtedValue = "";
-                // An output is produced in the console window asking if the user wants to run a test
-                Console.WriteLine("Do you want to run the test: Y or N");
-                // Try is used to ensure the user inputs a valid input and prevents any possible crashes
-                try
-                {
-                    // The users input is taken and is attempted to be stored as a string in InputtedValue
-                    inputtedValue = Console.ReadLine();
-                    // Sets the given input to uppercase
-                    inputtedValue = inputtedValue.ToUpper();
-                    // The input is compared with the two valid answers and if it does valid is set to true
-                    if (inputtedValue == "Y" || inputtedValue == "N")
-                    {
-                        valid = true;
-                    }
-                    else
-                    {
-                        throw new Exception();
-                    }
-                }
-                // If an error is produced in try no code will be executed repeating the while loop
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Invalid input, must be a character and be Y or X.");
-                }
-
-                // if the input was valid then the input is compared otherwise the code skips to the start of the while loop
-                if (valid)
-                {
-                    // Compares the players input and if the value matches run the nested code
-                    if (inputtedValue == "Y")
-                    {
-                        // Instaniates an object of the class testing
-                        Testing testing = new Testing();
-                        // the testing method test code is called
-                        testing.TestCode();
-                    }
-                    // Create a Game object and call the RollingContinous method
-                    Game game = new Game();
-                    game.RollingContinuous(true);
-                }
-            }
+            SevenOut game = new SevenOut();
+            game.SevenOutGame(false);
 
 
         }
