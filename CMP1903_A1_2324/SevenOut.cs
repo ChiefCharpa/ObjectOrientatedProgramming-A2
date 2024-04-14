385b00887452a012f6bf62c31543c875710da914
ChiefCharpa <145765779+ChiefCharpa@users.noreply.github.com>
2024-04-14 18:28:36 +0100

    update 6

--- CMP1903_A1_2324/SevenOut.cs
+++ CMP1903_A1_2324/SevenOut.cs
@@ -8,5 +8,71 @@ namespace CMP1903_A1_2324
 {
     internal class SevenOut
     {
+
+        private List<int> _storedRolls = new List<int> { };
+
+        public List<int> rollNumbers
+        {
+            get { return _storedRolls; }
+            set { _storedRolls = value; }
+        }
+
+        /*
+            Sevens Out
+            2 x dice
+            Rules:
+	            Roll the two dice, noting the total rolled each time.
+                 If it is a 7 - stop.
+                 If it is any other number - add it to your total.
+                    If it is a double - add double the total to your score(3,3 would add 12 to your total)
+        */
+
+
+        public int SevenOutGame(bool player)
+        {
+            rollNumbers = new List<int> { };
+            //Rolls 2 dice
+            Die die = new Die();
+            int dieTotal = 0;
+            bool gameStop = false;
+            int counter = 0;
+            while (!gameStop)
+            {
+                if (player)
+                {
+                    Console.WriteLine("Roll the dice");
+                    Console.ReadLine();
+                }
+                die.Roll();
+                counter++;
+                int DieRoll1 = die.rollNumber;
+                die.Roll();
+                int DieRoll2 = die.rollNumber;
+
+                Console.WriteLine($"Die pair {counter} rolled {DieRoll1} and {DieRoll2}");
+
+                if (DieRoll1 + DieRoll2 != 7)
+                {
+                    _storedRolls.Add(DieRoll1);
+                    _storedRolls.Add(DieRoll2);
+                    if (DieRoll1 == DieRoll2)
+                    {
+                        dieTotal = dieTotal + 2 * (DieRoll1 + DieRoll2);
+                    }
+                    else
+                    {
+                        dieTotal = dieTotal + (DieRoll1 + DieRoll2);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The pair totalled to be 7.");
+                    Console.WriteLine($"You scored a total of {dieTotal}");
+                }
+            }
+            return dieTotal;
+
+        }
+
     }
 }
