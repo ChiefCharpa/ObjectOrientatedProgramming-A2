2ceb51501dc97f242f40443cf9260ca2f003d70b
ChiefCharpa <145765779+ChiefCharpa@users.noreply.github.com>
2024-04-28 22:06:00 +0100

    up

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -13,10 +13,7 @@ namespace CMP1903_A1_2324
     internal class Game
     {
         /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
+         * MenuOutput acts to display the menu options to the user upon being called
          */
 
         private static void MenuOutput()
@@ -30,27 +27,42 @@ namespace CMP1903_A1_2324
             Console.WriteLine("x. to end application");
         }
 
+
+        /*
+         * ComputerOrPlayer is a private method that acts to get the users choice for playing against 
+         * a player or computer and performs whitelist validation on the input to prevent an error 
+         * from being raised
+         */
         private static string ComputerOrPlayer() 
         { 
+            // Invalid is set to true and player input is set to empty
             bool invalid = true;
             string playerInput = "";
+            // while invalid is true the player is asked to input 1 or 2 where the input will be stored
             while (invalid)
             {
                 Console.WriteLine("1. to play against a player");
                 Console.WriteLine("2. to play against the computer");
                 playerInput = Console.ReadLine();
+                // if the input is valid, then invalid is set to false 
                 if (playerInput == "1" || playerInput == "2")
                 {
                     invalid = false;
                 }
+                // if the input was invalid then an error message it outputted in the console window
                 else
                 {
                     Console.WriteLine("Input was invalid");
                 }
             }
+            // the users input is returned
             return playerInput;
         }
 
+
+        /*
+         * playerWin is a private static method that takes the players names and the array score to output the winner to the user in the console window 
+         */
         private static void playerWin(int[] score, string player1Name, string player2Name = "Computer")
         {
             if (score[0] == 0)
@@ -66,38 +78,51 @@ namespace CMP1903_A1_2324
                 Console.WriteLine($"{player2Name} has won. ");
             }
         }
-           
-        
 
-        
 
+
+
+        /*
+        * The main class acts to output the menu and call the relevent class depending on the users choice
+        */
         static void Main(string[] args)
         {
+            // an object of statistics is instantiated
             Statistics stats = new Statistics();
+            // A bool variable is set up to run the while loop until it is set to true
             bool end = false;
             while (end != true)
             {
+                // Calls the menu output method and stores the users input as val
                 MenuOutput();
                 string val = Console.ReadLine();
+                // whilst the input is valid
                 if (val != null)
                 {
+                    // if the input was 1 then an instance of Seven out is made as an object
                     if (val == "1")
                     {
                         SevenOut game = new SevenOut();
+                        // The method of ComputerOrPlayer is called and the value is stored in choice
                         string choice = ComputerOrPlayer();
+                        // if choice is 1 then the players are both prompted to enter a name which is stored
                         if (choice == "1")
                         {
                             Console.WriteLine("Enter first players name.");
                             string player1 = Console.ReadLine();
                             Console.WriteLine("Enter second players name.");
                             string player2 = Console.ReadLine();
+                            // Outputs to say it is player 1's turn and calls SevenOutGame in player mode and stores the value in player1Score
                             Console.WriteLine($"{player1}'s Turn. ");
                             int player1Score = game.SevenOutGame(true);
+                            // Outputs to say it is player 2's turn and calls SevenOutGame in player mode and stores the value in player2Score
                             Console.WriteLine($"{player2}'s Turn. ");
                             int player2Score = game.SevenOutGame(true);
+                            // Compares the players score and outputs the given winner or draw
                             if(player1Score > player2Score)
                             {
                                 Console.WriteLine($"{player1} wins. ");
+                                // if the value is greater than highscore a new value is set to the score and player name
                                 if (player1Score > stats.Highscore)
                                 {
                                     stats.Highscore = player1Score;
@@ -107,6 +132,7 @@ namespace CMP1903_A1_2324
                             else if(player2Score > player1Score)
                             {
                                 Console.WriteLine($"{player2} wins. ");
+                                // if the value is greater than highscore a new value is set to the score and player name
                                 if (player2Score > stats.Highscore)
                                 {
                                     stats.Highscore = player2Score;
@@ -117,20 +143,26 @@ namespace CMP1903_A1_2324
                             {
                                 Console.WriteLine("Draw. ");
                             }
+                            // Increments the total of games played
                             stats.GamesRan = stats.GamesRan + 1;
                         }
+                        // if choice is 2 then the player is prompted to enter a name which is stored
                         else
                         {
                             Console.WriteLine("Enter player's name.");
                             string player1 = Console.ReadLine();
+                            // Outputs to say it is player 1's turn and calls SevenOutGame in player mode and stores the value in player1Score
                             Console.WriteLine($"{player1}'s Turn. ");
                             int player1Score = game.SevenOutGame(true);
+                            // Outputs to say it is the computer's turn and calls SevenOutGame in computer mode and stores the value in player2Score
                             Console.WriteLine($"computer's Turn. ");
                             int player2Score = game.SevenOutGame(false);
+                            // Compares the players score and outputs the given winner or draw
                             if (player1Score > player2Score)
                             {
                                 Console.WriteLine($"{player1} wins. ");
-                                if(player1Score > stats.Highscore)
+                                // if the value is greater than highscore a new value is set to the score and player name
+                                if (player1Score > stats.Highscore)
                                 {
                                     stats.Highscore = player1Score;
                                     stats.TopPlayer = player1;
@@ -155,51 +187,72 @@ namespace CMP1903_A1_2324
                                     stats.TopPlayer = player1;
                                 }
                             }
+                            // Increments the total of games played
                             stats.GamesRan = stats.GamesRan + 1;
                         }
 
                     }
+                    // if the input was 1 then an instance of ThreeOrMore is made as an object
                     else if (val == "2")
                     {
+                        // The method of ComputerOrPlayer is called and the value is stored in choice
                         string choice = ComputerOrPlayer();
                         ThreeOrMore game = new ThreeOrMore();
+                        // if choice is 1 then the players are both prompted to enter a name which is stored
                         if (choice == "1")
                         {
                             Console.WriteLine("Enter first players name.");
                             string player1 = Console.ReadLine();
                             Console.WriteLine("Enter second players name.");
                             string player2 = Console.ReadLine();
+                            // The method ThreeOrMoreGame is called in 2 player mode
                             int[] outcome = game.ThreeOrMoreGame(true);
+                            // Games ran is incremented
                             stats.GamesRan = stats.GamesRan + 1;
+                            // playerWin is called passing the array and 2 players names
                             playerWin(outcome, player1, player2);
                         }
+                        // if choice is 2 then the player is prompted to enter a name which is stored
                         else
                         {
                             Console.WriteLine("Enter first players name.");
                             string player1 = Console.ReadLine();
+                            // The method ThreeOrMoreGame is called in 2 player mode
                             int[] outcome = game.ThreeOrMoreGame(false);
+                            // Games ran is incremented
                             stats.GamesRan = stats.GamesRan + 1;
+                            // playerWin is called passing the array and players name
                             playerWin(outcome, player1);
                         }
 
                     }
+                    // if val is 3 then call the method display stats
                     else if (val == "3")
                     {
                         stats.DisplayStats();
                     }
+                    // if val is 4 then call method TestCode
                     else if(val == "4")
                     {
                         Testing.TestCode();
                     }
+                    // if value is x then set end to be true
                     else if (val == "x")
                     {
                         end = true;
                     }
+                    // output error for invalid input
                     else
                     {
                         Console.WriteLine("Invalid input. ");
                     }
 
+                }
+                // output error for input entered
+                // output error for input entered
+                else
+                {
+                    Console.WriteLine("No input was entered. ");
                 }
             }
 
